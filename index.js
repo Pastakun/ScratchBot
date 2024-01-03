08c1e5018234ac5b5fd0af376a8b19c652f8d089
Pastakun <130649332+Pastakun@users.noreply.github.com>
2024-01-03 11:23:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -94,12 +94,6 @@ async function main() {
             socket.send("".concat('{"method":"handshake","user":"noodle_910","project_id":"945955173"}', "\n"));
         });
         socket.on('message', (data) => {
-            data.split("\n").forEach((function(e) {
-                if (e) {
-                    console.log(JSON.parse(e));
-                }
-            }
-            ))
             console.log('サーバーからメッセージを受信:', data);
         });
     } catch (error) {
